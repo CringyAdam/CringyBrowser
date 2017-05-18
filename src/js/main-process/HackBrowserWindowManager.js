52e221d5d227ac77bd5b0553cee1015203f0aa55
Adam <adam.aharony@icloud.com>
2017-05-18 18:58:11 +0300

    Add files via upload

---
+++ src/js/main-process/HackBrowserWindowManager.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const electron = require('electron');
+const path = require('path');
+const BrowserWindow = electron.BrowserWindow;
+const PersistentStorage = require(path.join(__app.srcPath, 'js', 'common', 'PersistentStorage'));
+var logger = global.__app.logger;
+
+/**
+ * HackBrowserWindowManager handles opening and closing of browser windows
+ *
+ * @constructor
+ */
+var HackBrowserWindowManager = {};
+
+var windowList = {};
+var createdWindowCount = 0;
+
+
+
+
+// TODO: navigate to specified url
+HackBrowserWindowManager.openNewWindow = function(url) {
+	var _this = this;
+
+	// get last browser size
+	PersistentStorage.getItem('browserWindowSize', function(err, browserSize) {
+		if (err) {
+			browserSize = {
+				width: 800,
+				height: 600
+			};
+
+			logger.debug('Could not get browserWindowSize from PersistentStorage');
+			logger.debug(browserSize);
+		} else {
+			logger.debug('Got browserWindowSize from PersistentStorage');
+			logger.debug(browserSize);
+		}
+
+		// create the browser window
+		var newWindow = new BrowserWindow({
+			width: browserSize.width,
+			height: browserSize.height,
+			frame: true
+		});
+
+		newWindow.initialURL = url;
+
+		// TODO: Use path.join or some other URL concat method
+		// load the HTML file for browser window
+		newWindow.loadURL("file://" + __app.srcPath + "/html-pages/browser-window.html");
+
+		// Open the DevTools (debugging)
+		windowList[newWindow.id] = newWindow;
+
+		_this.attachEventHandlers(newWindow);
+
+		// increase window count
+		createdWindowCount++;
+	});
+};
+
+HackBrowserWindowManager.attachEventHandlers = function(browserWindow) {
+	var _this = this;
+
+	var windowId = browserWindow.id;
+
+	// save browser window's width/height when user closes it
+	browserWindow.on('close', function() {
+		var size = browserWindow.getSize();
+
+		var sizeObject = {
+			'width': size[0],
+			'height': size[1]
+		};
+
+		// save to persistent storage
+		PersistentStorage.setItem('browserWindowSize', sizeObject);
+	});
+
+	// remove the window from windowList and remove reference so that GC can clear it from memory
+	browserWindow.on('closed', function() {
+		if (windowList.hasOwnProperty(windowId)) {
+			console.log('deleting window ' + windowId);
+
+			delete windowList[windowId];
+			browserWindow = null;
+		}
+	});
+};
+
+module.exports = HackBrowserWindowManager;
---
+++ src/js/main-process/MainProcessController.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const {dialog} = require('electron');
+const fs = require('fs');
+const path = require('path');
+const EventEmitter = require('events').EventEmitter;
+const session = require('electron').session;
+const winston = require('winston');
+
+const hackBrowserWindowManager = require(path.join(global.__app.srcPath, 'js', 'main-process', 'HackBrowserWindowManager'));
+const GlobalShortcutHandler = require(path.join(global.__app.srcPath, 'js', 'main-process', 'GlobalShortcutHandler'));
+const IPCMainProcessHandler = require(path.join(global.__app.srcPath, 'js', 'main-process', 'IPCMainProcessHandler'));
+
+function MainProcessController() {
+	var _this = this;
+
+	var ipcHandler;
+	var mainProcessEventEmitter;
+
+	var init = function() {
+		// attempt to enable Pepper Flash Player plugin
+		// binary for pepper flash file is saved in {src-directory}/binaries/
+		enablePepperFlashPlayer();
+
+		attachEventHandlers();
+	};
+
+	var attachEventHandlers = function() {
+		app.on('window-all-closed', function() {
+			console.log('window-all-closed, quitting');
+
+			if (process.platform != 'darwin') {
+				console.log('quitting app');
+
+				app.quit();
+			}
+		});
+
+		app.on('ready', function() {
+			session.defaultSession.on('will-download', handleWillDownload);
+
+			// check if .data directory exists
+			fs.exists(global.__app.dataPath, function(exists) {
+				if (exists === false) {
+					// create directory if .data directory doesn't exist
+					// TODO: check directory create permissions on Linux
+					fs.mkdir(global.__app.dataPath, function(err) {
+						if (err) {
+							// TODO: show error messagebox and quit src
+							dialog.showMessageBox({
+								type: 'info',
+								buttons: ["ok"],
+								title: global.__app.dataPath,
+								message: JSON.stringify(err),
+								detail: JSON.stringify(err)
+							});
+
+							app.quit();
+						} else {
+							startBrowser();
+						}
+					});
+				} else {
+					startBrowser();
+				}
+			});
+		});
+	};
+
+	/**
+	 * attempt to enable flash with Pepper Flash player
+	 *
+	 * supported OS versions
+	 * Windows 7 64-bit, Mac OS 64-bit
+	 */
+	var enablePepperFlashPlayer = function() {
+		var ppapi_flash_path = null;
+
+		// specify flash path based on OS
+		if(process.platform  == 'win32'){
+			ppapi_flash_path = path.join(global.__app.srcPath, 'binaries', 'pepflashplayer64_25_0_0_127.dll');
+		} else if (process.platform == 'darwin') {
+			// Mac OS
+			ppapi_flash_path = path.join(global.__app.srcPath, 'binaries', 'PepperFlashPlayer.plugin');
+		}
+
+		// in case ppapi_flash_path is set
+		if (ppapi_flash_path) {
+			app.commandLine.appendSwitch('ppapi-flash-path', ppapi_flash_path);
+			app.commandLine.appendSwitch('ppapi-flash-version', '25.0.0.127');
+		}
+	};
+
+	/**
+	 * Start the browser and open a new window
+	 */
+	var startBrowser = function() {
+		// create a shared EventEmitter for windowManager to communicate with ipcHandler
+		mainProcessEventEmitter = new EventEmitter();
+		ipcHandler = new IPCMainProcessHandler(_this);
+
+		var shortcutHandler = new GlobalShortcutHandler(hackBrowserWindowManager);
+
+		// register all global shortcuts
+		shortcutHandler.registerAll();
+
+		hackBrowserWindowManager.openNewWindow();
+	};
+
+	var handleWillDownload = function(event, item, webContents) {
+		item.on("done", function(e, state) {
+			if (state === 'completed') {
+				var itemInfoObj = {
+					type: 'file-download',
+					fileSize: item.getTotalBytes(),
+					fileURL: item.getURL(),
+					fileName: item.getFilename(),
+					fileMimeType: item.getMimeType()
+				};
+			}
+		});
+	};
+
+	_this.start = function() {
+		init();
+	};
+
+	_this.getMainProcessEventEmitter = function() {
+		return mainProcessEventEmitter;
+	};
+
+	_this.getWindowManager = function() {
+		return hackBrowserWindowManager;
+	};
+}
+
+module.exports = MainProcessController;
